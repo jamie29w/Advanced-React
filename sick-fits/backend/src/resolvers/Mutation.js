c2c5af77bdbf9a0d483674e0972b47abf44162f8
Jamie Woodmancy <jamie29w@gmail.com>
2018-10-21 21:33:55 -0700

    built out first dummy items in backend; set up Apollo to query backend from React

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,25 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    //create a dog
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // check if they're logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
   }
+  // createDog(parent, args, ctx, info) {
+  //   //create a dog
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = Mutations;
