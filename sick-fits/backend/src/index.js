7c86b0804901f4c40278045443acf9a17e262fb2
Jamie Woodmancy <jamie29w@gmail.com>
2018-10-21 19:17:37 -0700

    got Prisma server set up and created first GraphQL query and mutation

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+//TODO use epxress mdlwr to handle cookies (JWT)
+// TODO use express mdlwr to populate current users
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  res =>
+    console.log(`server is now running on port https:/localhost:${res.port}`)
+);
